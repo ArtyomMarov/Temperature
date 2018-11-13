914712f8da37fd468cc54eba5214ab0812cf454a
ArtyomMarov <ArtyomMarow@gmail.com>
2018-11-13 11:44:35 +0300

    Создал ошибки в файле тестов. тесты не должны пройти.

--- tst_myfirstunittest.cpp
+++ tst_myfirstunittest.cpp
@@ -34,14 +34,14 @@ void MyFirstUnitTest::testFahrenheitToCelsius()
 {
     MyClass myClass;
     QCOMPARE(myClass.fahrenheitToCelsius(32.0), 0.0);
-    QCOMPARE(myClass.fahrenheitToCelsius(0.0), -17.8);
+    QCOMPARE(myClass.fahrenheitToCelsius(0.0), -20);
     QCOMPARE(myClass.fahrenheitToCelsius(100.0), 37.8);
     QCOMPARE(myClass.fahrenheitToCelsius(50.0), 10.0);
 }
 
 void MyFirstUnitTest::testCase1()
 {
-    QVERIFY2(true, "Failure");
+    QVERIFY2(2>3, "Failure");
 }
 
 void MyFirstUnitTest::tableTestFahrenheitToCelsius_data()
@@ -51,11 +51,11 @@ void MyFirstUnitTest::tableTestFahrenheitToCelsius_data()
 
     QTest::newRow("Test data 1") << 0.0 << -17.8;
     QTest::newRow("Test data 2") << 10.0 << -12.2;
-    QTest::newRow("Test data 3") << 20.0 << -6.7;
+    QTest::newRow("Test data 3") << 20.0 << -6.8;
     QTest::newRow("Test data 4") << 32.0 << 0.0;
     QTest::newRow("Test data 5") << 40.0 << 4.4;
     QTest::newRow("Test data 6") << 50.0 << 10.0;
-    QTest::newRow("Test data 7") << 60.0 << 15.6;
+    QTest::newRow("Test data 7") << 60.0 << 15.5;
 }
 
 void MyFirstUnitTest::tableTestFahrenheitToCelsius()
